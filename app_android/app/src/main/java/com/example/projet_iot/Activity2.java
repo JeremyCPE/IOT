96292b52999bfc53fe8c76cc0ee5e26519f9f7f8
tesconnecte <alexis.canevali@gmail.com>
2019-11-14 17:30:18 +0100

    les modifs du petit simon

--- app_android/app/src/main/java/com/example/projet_iot/Activity2.java
+++ app_android/app/src/main/java/com/example/projet_iot/Activity2.java
@@ -10,15 +10,27 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 public class Activity2 extends AppCompatActivity {
     private String ip;
     private int port;
+    private SenderTask st;
+    private InetAddress ia;
+    private DatagramSocket UDPSocket;
+    private InetAddress address;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
-        this.ip = this.getIntent().getStringExtra("IP_ADDR");
-        this.port = this.getIntent().getIntExtra("PORT",8080);
+        //this.ip = this.getIntent().getStringExtra("IP_ADDR");
+        //this.port = this.getIntent().getIntExtra("PORT",8080);
+        this.ip = "192.168.1.12";
+        this.port = 10000;
         final Button firstDownButton = findViewById(R.id.firstDownButton);
         final Button secondUpButton = findViewById(R.id.secondUpButton);
         final Button secondDownButton = findViewById(R.id.secondDownButton);
@@ -49,11 +61,32 @@ public class Activity2 extends AppCompatActivity {
             }
         });
 
-        diffuseButton.setOnClickListener(new View.OnClickListener() {
+        /*diffuseButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                sendData(firstText,secondText,thirdText);
+
             }
-        });
+        });*/
+        SenderTask st = new SenderTask();
+        try {
+            ia = InetAddress.getByName(this.ip);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        (new Thread(){
+            public void run(){
+                try {
+                    byte[] reset = "(0)".getBytes();
+                    UDPSocket = new DatagramSocket();
+                    (new ReceiverTask(UDPSocket, 10000)).execute();
+                    address = InetAddress.getByName("192.168.1.12");
+                    UDPSocket.send(new DatagramPacket(reset, reset.length, address, Integer.parseInt("10000")));
+                    Log.println(Log.ASSERT, "action", "reset");
+
+                } catch (IOException e) {
+                    Log.println(Log.ASSERT, "network", e.toString());
+                }
+            }
+        }).start();
     }
 
     private void changeText(TextView firstText, TextView secondText) {
@@ -62,13 +95,24 @@ public class Activity2 extends AppCompatActivity {
         secondText.setText(tampon);
     }
 
-    public void sendData(TextView firstText, TextView secondText, TextView thirdText) {
-        //TODO : IMPLEMENT ALGORITHM TO SEND DATA
+    public void testEnvoyerVersServeur(View view){
+        TextView tv1 = (TextView) findViewById(R.id.firstText);
+        TextView tv2 = (TextView) findViewById(R.id.secondText);
+        TextView tv3 = (TextView) findViewById(R.id.thirdText);
 
-      String txt1 =  firstText.getText().toString().substring(0,1);
-      String txt2 =  secondText.getText().toString().substring(0,1);
-      String txt3 =  thirdText.getText().toString().substring(0,1);
-        String tabToSend[] = {txt1,txt2,txt3};
-        Log.d("Test",txt1 + "\n" + txt2 + "\n" + txt3 );
+        //sendData();
+
+        String txt1 =  tv1.getText().toString().substring(0,1);
+        String txt2 =  tv2.getText().toString().substring(0,1);
+        String txt3 =  tv3.getText().toString().substring(0,1);
+
+        sendData(txt1+txt2+txt3);
+    }
+
+    public void sendData(String data) {
+        //TODO : IMPLEMENT ALGORITHM TO SEND DATA
+        //String tabToSend[] = {txt1,txt2,txt3};
+        //Log.d("Test",txt1 + "\n" + txt2 + "\n" + txt3 );
+        st.UDPSend(data,this.ia,this.port);
     }
 }
--- app_android/app/src/main/java/com/example/projet_iot/ReceiverTask.java
+++ app_android/app/src/main/java/com/example/projet_iot/ReceiverTask.java
@@ -1,36 +1,50 @@
 package com.example.projet_iot;
 
 import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.net.UnknownHostException;
+import java.net.DatagramSocket;
 
-public class ReceiverTask extends AsyncTask<Void, String, Void> {
-    private static InetAddress address;             // Structure Java décrivant une adresse résolue
-    private static int port;
+
+public class ReceiverTask extends AsyncTask<Void, byte[], Void> {
     private DatagramSocket UDPSocket;
+    private final String IP = "";
+    private final int PORT;
+    private InetAddress address;
 
-
-    public ReceiverTask(String ip_addr, int port) {
-        byte[] biteAdress = ip_addr.getBytes();
-        try {
-            this.address = InetAddress.getByAddress(biteAdress);
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
-        }
-        this.port = port;
+    ReceiverTask(DatagramSocket socket, int port){
+        this.UDPSocket = socket;
+        this.PORT = port;
     }
 
     @Override
-    protected Void doInBackground(Void... voids) {
-        while(true){ // Boucle d'attente active de
-            //messagesString msgRecu= ... ;
-            // attente d'un message sur le réseau
-            //publishProgress(msgRecu); // Publication asychrone du résultat}
+    protected Void doInBackground(Void... rien) {
+
+        while(true){
+            byte[] data = new byte [256];
+            DatagramPacket packet = new DatagramPacket(data, data.length);
+            try {
+                UDPSocket.receive(packet);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            int size = packet.getLength();
+            data = packet.getData();
+            if (data.length>0){
+                System.out.println(data);
+            }
+
         }
     }
-    protected void onProgressUpdate(String... data) {
-        //displayInputs(data[0]);
+
+    protected  void onProgessUpdate(byte[]... data){
+        Log.println(Log.ASSERT, "action", data.toString());
     }
 }
